39c8a80b8725cbfb792d5839b1af0525d8885145
Santos <santos@gmail.com>
2020-04-21 15:08:18 -0300

    First commit :D

--- core/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-class JurosForm(forms.Form):
-    montante = forms.FloatField()
-    capital = forms.FloatField()
-    taxa_de_juros = forms.FloatField()
-    tempo = forms.IntegerField()
--- core/views.py
+++ core/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
-from .forms import JurosForm
 
 
 def index(request):
-    form = JurosForm()
-    montante = request.POST.get('montante', '')
-    capital = request.POST.get('capital', '')
-    taxa_de_juros = request.POST.get('taxa_de_juros', '')
-    tempo = request.POST.get('tempo', '')
-    context = {'form': form}
-    return render(request, 'core/index.html',context)
+    # form = JurosForm()
+    # montante = request.POST.get('montante', '')
+    # capital = request.POST.get('capital', '')
+    # taxa_de_juros = request.POST.get('taxa_de_juros', '')
+    # tempo = request.POST.get('tempo', '')
+    # context = {'form': form}
+    return render(request, 'core/index.html')
